eed50c08adf885512f7598c33ccf7e21b7b97137
Jameskim <moduedu@gmail.com>
2020-06-05 17:50:04 +0900

    Data JSON 처리

--- server.js
+++ server.js
@@ -4,15 +4,19 @@ var app = express()
 
 var path = require('path')
 var fs = require('fs')
+var queryAllCar = require('./util')
 
 app.set('views' , path.resolve(__dirname ,'views'))
 app.set('view engine' , 'ejs')
-app.get('/' , (req, res)=>{
-
-    res.render('index')
+app.get('/' ,async (req, res)=>{
+    var data = await queryAllCar()
+    var resData = await JSON.parse(data)
+    await console.log(data)
+    await console.log(resData)
+    res.render('index' , {data:resData})
 })
 
 
-app.listen(8080 , ()=>{
+app.listen(8000 , ()=>{
     console.log("Server is Starting at http://localhost:8080")
 })
